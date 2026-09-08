90deff23a69dbb49772a5c84f2c183b0b86c12d2
Souad5 <souadalkabir@gmail.com>
2026-09-08 22:17:20 +0600

    feat: add express request type augmentation

---
+++ src/@types/index.d.ts
@@ -0,0 +1,17 @@
+import type { Role, UserStatus } from '@prisma/client';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        name: string;
+        role: Role;
+        status: UserStatus;
+      };
+    }
+  }
+}
+
+export {};
